206e6e3b2c629a6f6a11f953cdd5bc3a7a6a39ed
Poke <antoine.gagnon760@hotmail.com>
2018-11-21 15:40:55 -0500

    add codtracker mention

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import { Icon } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Icon } from "semantic-ui-react";
 
 class Footer extends Component {
   render() {
     return (
       <footer className="col12 colm12 colt12">
-        <a href="http://www.github.com/Poke1650/bo4stats">Fork on Github <Icon name="github" size="large"/></a>
+        <a href="http://www.github.com/Poke1650/bo4stats">
+          Fork on Github <Icon name="github" size="large" />
+        </a>{" "}
+        - Powered by&nbsp;{" "}
+        <a href="https://callofdutytracker.com/site-api">
+          callofdutytracker.com
+        </a>
       </footer>
     );
   }
